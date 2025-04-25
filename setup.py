99fadff8c183af9bb00b3f8a602a8e77c20eb02d
Rifkhi Akbar Rustandi <rakbarputrar@gmail.com>
2025-04-25 14:13:58 +0700

    feat: added setup.py for whl

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+__version = "1.0.0"
+install_requires = [
+    "requests",
+    "packaging",
+    "logging"
+]
+tests_require = []
+
+spec = {
+    "name": "oc-logging",
+    "version": __version,
+    "license": "Apache2.0",
+    "description": "Custom Development python API logging libraries",
+    "long_description": "Custom Development python API logging libraries",
+    "long_description_content_type": "text/plain",
+    "packages": ["oc_logging"],
+    "install_requires": install_requires,
+    "tests_require": tests_require,
+    "python_requires": ">=3.6",
+}
+
+setup(**spec)
