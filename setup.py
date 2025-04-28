e6c8afeb36b9f33d24a24a7f28db3ee731d3b412
Rifkhi Akbar Putra Rustandi <151907773+rifkhia@users.noreply.github.com>
2025-04-28 11:34:29 +0700

    feat/add new logging library (#1)
    * feat: add workflows for pushing the library to pypi
    
    * feat: add logging library to generalize all python logs
    
    * feat: add unittest for logging library
    
    * feat: added setup.py for whl
    
    * feat: add readme.md
    
    * feat: remove the packages section
    
    * feat: remove logging since it builtin
    
    * feat: supress py.warnings to write log
    
    * feat: increment version

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+__version = "1.0.1"
+install_requires = [
+    "requests",
+    "packaging",
+]
+tests_require = []
+
+spec = {
+    "name": "oc-logging",
+    "version": __version,
+    "license": "Apache2.0",
+    "description": "Custom Development python API logging libraries",
+    "long_description": "Custom Development python API logging libraries",
+    "long_description_content_type": "text/plain",
+    "install_requires": install_requires,
+    "tests_require": tests_require,
+    "python_requires": ">=3.6",
+}
+
+setup(**spec)
